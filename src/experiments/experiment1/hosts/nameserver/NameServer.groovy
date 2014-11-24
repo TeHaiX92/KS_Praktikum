580f7d76a226ea20d3f92180fdd397edb4cb18eb
TeHaiX92 <tehaix92@gmail.com>
2014-11-24 18:20:55 +0100

    Test

--- src/experiments/experiment1/hosts/nameserver/NameServer.groovy
+++ src/experiments/experiment1/hosts/nameserver/NameServer.groovy
@@ -7,66 +7,80 @@ import common.utils.Utils
  */
 class NameServer {
 
-    //========================================================================================================
-    // Vereinbarungen ANFANG
-    //========================================================================================================
+	//========================================================================================================
+	// Vereinbarungen ANFANG
+	//========================================================================================================
 
-    /** Der Netzwerk-Protokoll-Stack */
-    experiments.experiment1.stack.Stack stack
+	/** Der Netzwerk-Protokoll-Stack */
+	experiments.experiment1.stack.Stack stack
 
-    /** Konfigurations-Objekt */
-    ConfigObject config
+	/** Konfigurations-Objekt */
+	ConfigObject config
 
-    /** Stoppen der Threads wenn false */
-    Boolean run = true
+	/** Stoppen der Threads wenn false */
+	Boolean run = true
 
-    /** Tabelle zur Umsetzung von Namen in IP-Adressen */
-    Map<String, String> nameTable = [
-            "meinhttpserver": "0.0.0.0",
-            "alice": "0.0.0.0",
-            "bob": "0.0.0.0",
-    ]
+	/** Tabelle zur Umsetzung von Namen in IP-Adressen */
+	Map<String, String> nameTable = [
+		"meinhttpserver": "192.168.1.10",
+		"alice"         : "0.0.0.0",
+		"bob"           : "0.0.0.0",
+	]
 
-    //========================================================================================================
-    // Methoden ANFANG
-    //========================================================================================================
+	//========================================================================================================
+	// Methoden ANFANG
+	//========================================================================================================
 
-    //------------------------------------------------------------------------------
-    /**
-     * Start der Anwendung
-     */
-    static void main(String[] args) {
-        // Client-Klasse instanziieren
-        NameServer application = new NameServer()
-        // und starten
-        application.nameserver()
-    }
-    //------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------
+	/**
+	 * Start der Anwendung
+	 */
+	static void main(String[] args) {
+		// Client-Klasse instanziieren
+		NameServer application = new NameServer()
+		// und starten
+		application.nameserver()
+	}
+	//------------------------------------------------------------------------------
 
-    /**
-     * Der Namens-Dienst
-     */
-    void nameserver() {
+	/**
+	 * Der Namens-Dienst
+	 */
+	void nameserver() {
 
-        //------------------------------------------------
+		//------------------------------------------------
 
-        // Konfiguration holen
-        config = Utils.getConfig("experiment1", "nameserver")
+		// Konfiguration holen
+		config = Utils.getConfig("experiment1", "nameserver")
 
-        // ------------------------------------------------------------
+		// ------------------------------------------------------------
 
-        // Netzwerkstack initialisieren
-        stack = new experiments.experiment1.stack.Stack()
-        stack.start(config)
+		// Netzwerkstack initialisieren
+		stack = new experiments.experiment1.stack.Stack()
+		stack.start(config)
 
-        Utils.writeLog("NameServer", "nameserver", "startet", 1)
+		Utils.writeLog("NameServer", "nameserver", "startet", 1)
 
-        while (run) {
-            // Hier Protokoll implementieren:
-            // auf Empfang ueber UDP warten
-            // Namen über nameTable in IP-Adresse aufloesen
-            // IP-Adresse ueber UDP zuruecksenden
-        }
-    }
-    //------------------------------------------------------------------------------
+		while (run) {
+			// Hier Protokoll implementieren:
+			// auf Empfang ueber UDP warten
+			(srcIpAddr, srcPort, data) = stack.udpReceive()
+
+			// Namen über nameTable in IP-Adresse aufloesen
+			Utils.writeLog("NameServer", "nameserver", "Aufzuloesende Anfrage ${data} wurde erhalten", 1)
+			String ipAddr = nameTable.get(data)
+			if (ipAddr) {
+				Utils.writeLog("NameServer", "nameserver", "Aufgeloest in $ipAddr", 1)
+			} else {
+				Utils.writeLog("NameServer", "nameserver", "Fehler: Host $data wurde nicht gefunden", 1)
+				ipAddr = "0.0.0.0"
+			}
+
+			String answer = "ANTWORT:" + ipAddr
+
+			// IP-Adresse ueber UDP zuruecksenden
+			stack.udpSend(dstIpAddr: srcIpAddr, dstPort: srcPort, srcPort: config.ownPort, sdu: answer)
+		}
+	}
+	//------------------------------------------------------------------------------
 }
