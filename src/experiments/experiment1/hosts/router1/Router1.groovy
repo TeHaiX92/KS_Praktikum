40c7f20cb8cffa88a23f51ff45f95358942659d8
aprang <aprang92@yahoo.de>
2015-02-10 16:55:30 +0100

    Router klappt alles nicht

--- src/experiments/experiment1/hosts/router1/Router1.groovy
+++ src/experiments/experiment1/hosts/router1/Router1.groovy
@@ -1,4 +1,4 @@
-package experiments.experiment1.hosts.router4
+package experiments.experiment1.hosts.router1
 
 import common.utils.Utils
 
@@ -62,6 +62,16 @@ class Router1 {
 		config = Utils.getConfig("experiment1", "router1")
 		neighborTable = config.neighborTable
 
+		// Nachbarn in ANT übernehmen mit false und 0
+
+		int i = 0
+		for(List entry in neighborTable) {
+			List neighbor = [entry[0],entry[1],false,0,i++]
+			aliveNeighborTable.add(neighbor)
+		}
+
+		printANT()
+
 		// ------------------------------------------------------------
 
 		// Netzwerkstack initialisieren
@@ -77,13 +87,21 @@ class Router1 {
 
 		Utils.writeLog("Router", "router1", "startet", 1)
 
-		sendAliveRequest()
-
 		while (run) {
+
+			sendAliveRequest()
+			getAliveRequest()
+
+			getAliveReply()
+			// TTL verringern
+			decreaseNeighborTTL()
+
 			// Periodisches Versenden von Routinginformationen
-			neighborTTLDec()
 			sendPeriodical()
+
 			sleep(config.periodRInfo)
+
+			// Status anfragen
 		}
 	}
 
@@ -104,125 +122,10 @@ class Router1 {
 		/** Empfangene Routinginformationen */
 		String rInfo
 
-		while(run) {
-
-			// Auf UDP-Empfang warten
-			(iPAddr, port, rInfo) =  stack.udpReceive()
-
-			if (rInfo.equals("alive")) {
-
-				boolean found = false
-				List neighbor = aliveNeighborTable.find { entry -> entry[0].equals(iPAddr) }
-
-				//
-				if (neighbor != null) {
-					found = true
-					neighbor[2] = true
-					neighbor[3] = 3 // Time to Live auf 3 Runden
-				}
-
-				// Neuer Nachbarn gefunden
-				if (!found) {
-					List l = [iPAddr, port, true, 3]
-					aliveNeighborTable.add(l)
-				}
-			}
-			else {
-				// Jetzt aktuelle Routingtablle holen:
-				List rt = stack.getRoutingTable()
-
-				// neue Routinginformationen bestimmen
-
-				boolean rt_changed = false
-				String[] neighbourRouting = rInfo.tokenize('[')
-				List<List> neighbourRoutingTable = []
-
-				neighbourRouting.each { item ->
-					item -= ' '
-					item -= ']'
-					List<String> l = []
-					item.tokenize(',').each { str -> l.add(str.trim()) }
-					neighbourRoutingTable.add(l)
-				}
-
-				// Vergleiche die eigenen Routing-Einträge mit den übermittelten
-				// Falls die IP inkl. Maske bisher nicht vorhanden ist, erstelle einen neuen Eintrag
-
-				int toNieghborMetric = 1000
-				List dstToNieghborList = rt.find { ownEntry -> // should be findAll
-					Utils.getNetworkId(iPAddr, ownEntry[1]).equals(ownEntry[0])
-				}
-
-				if (dstToNieghborList != null)
-					toNieghborMetric = dstToNieghborList[4].toInteger()
-
-
-				rt.each { ownEntry ->
-					String dstIP = Utils.getNetworkId(ownEntry[0], ownEntry[1])
-					int dstIpMetric = ownEntry[4].toInteger()
-
-					// println(Utils.getNetworkId(neighbourRoutingTable[0][1].toString(),neighbourRoutingTable[0][0].toString()))
-					List nbDst = neighbourRoutingTable.find { nbEntry -> dstIP.equals(Utils.getNetworkId(ownEntry[1].toString(), nbEntry[0].toString())) }
-					boolean isNeighbor = (neighborTable.find{entry -> entry[0].equals(dstIP)}!=null)
-					int nbDstMetric = 1000
-					if(nbDst!=null&&(!isNeighbor)){
-						nbDstMetric = nbDst[4].toInteger()
-					}
-					if ((nbDstMetric + toNieghborMetric) < dstIpMetric) {
-						ownEntry[4] = nbDstMetric + toNieghborMetric
-						ownEntry[3] = dstToNieghborList[3]
-						ownEntry[2] = iPAddr
-						rt_changed = true
-
-						println("router 4 with help from $iPAddr found a better path to $dstIP")
-					}
-				}
-
-				//    zum Zerlegen einer Zeichenkette siehe "tokenize()"
-				// extrahieren von Information, dann iInfo als !Zeichenkette! erzeugen ...
-
-				// Routingtabelle an Vermittlungsschicht uebergeben:
-				stack.setRoutingTable(rt)
-
-				// und neue Routinginformationen verteilen:
-				String outrInfo = rt.join(",")
-				if (rt_changed){
-					sendToNeigbors(outrInfo)
-					String strout =""
-					rt.each{item -> strout += item + "\n"}
-					println("the new table of router 4 is:\n $strout")
-				}
-				// oder periodisch verteilen lassen
-
-			}
-		}
+		// Auf UDP-Empfang warten
+		(iPAddr, port, rInfo) =  stack.udpReceive()
 	}
 
-	/**
-	 * Verringern der Time to Live (TTL)
-	 */
-	void neighborTTLDec(){
-		List rt = stack.getRoutingTable()
-
-		// Untersuchung aller Nachbarn in der ANT
-		for(List neighbor in aliveNeighborTable) {
-
-			// Wenn Nachbar noch am Leben, dann TTL verringern
-			if (neighbor[3] > 0) {
-				neighbor[3]--
-			}
-			// Ansonsten ist der Nachbar tot
-			else {
-				rt.each { entry ->
-					if (entry[2].toString().equals(neighbor[0])) {
-						neighbor[2] = false
-					}
-				}
-			}
-		}
-
-		stack.setRoutingTable(rt)
-	}
 	// ------------------------------------------------------------
 
 	/** Periodisches Senden der Routinginformationen */
@@ -231,7 +134,7 @@ class Router1 {
 
 		String rInfo = ""
 
-		for (entry in routingTable) {
+		for (List entry in routingTable) {
 			rInfo = rInfo + "${entry[0]}, ${entry[1]}, ${entry[2]}, ${entry[3]}; "
 		}
 
@@ -239,7 +142,6 @@ class Router1 {
 
 		// Zum Senden uebergeben
 		sendToNeigbors(rInfo)
-		sendAliveRequest()
 	}
 
 	// ------------------------------------------------------------
@@ -257,12 +159,94 @@ class Router1 {
 
 	//------------------------------------------------------------------------------
 
-	/**
-	 * Sende Alive-Request
-	 */
-	void sendAliveRequest(){
+	void printANT() {
+		for(entry in aliveNeighborTable) {
+			println("IP: ${entry[0]}, Port: ${entry[1]}, isAlive: ${entry[2]}, TTL: ${entry[3]}, Pos: ${entry[4]}")
+		}
+	}
+
+	void sendAliveRequest() {
 		for (List neigbor in neighborTable) {
+			//Utils.writeLog("Router 1", "send", "\u001B[32mAliveRequest\u001B[0m", 1)
+			//println("\u001B[32mAliveRequest\u001B[0m")
 			stack.udpSend(dstIpAddr: neigbor[0], dstPort: neigbor[1], srcPort: config.ownPort, sdu: "alive")
+			Utils.writeLog("Router 1", "send", "\u001B[32mAliveRequest\u001B[0m", 1)
+		}
+	}
+
+	void getAliveRequest() {
+		/** IP-Adresse des Nachbarrouters */
+		String iPAddr
+
+		/** UDP-Portnummer des Nachbarrouters */
+		int port
+
+		/** Empfangene Routinginformationen */
+		String rInfo
+
+		// Auf UDP-Empfang warten
+		(iPAddr, port, rInfo) =  stack.udpReceive()
+
+		if (rInfo.equals("alive")) {
+			sendAliveReply(iPAddr, port)
+			Utils.writeLog("Router 1", "receive", "\u001B[32mAliveRequest\u001B[0m", 1)
+		}
+	}
+
+	void sendAliveReply(String IpAddr, int port) {
+		stack.udpSend(dstIpAddr: IpAddr, dstPort: port, srcPort: config.ownPort, sdu: "ok")
+		Utils.writeLog("Router 1", "send", "\u001B[32mAliveReply\u001B[0m", 1)
+	}
+
+	void getAliveReply() {
+		String iPAddr
+		int port
+		String rInfo
+
+		// Auf UDP-Empfang warten
+		(iPAddr, port, rInfo) =  stack.udpReceive()
+
+		if (rInfo.equals("ok")) {
+
+			// Nachbarrouter ausfindig machen
+			List neighbor = aliveNeighborTable.find { entry -> entry[0].equals(iPAddr) }
+
+			// Nachbarrouter aktualisieren
+			if (neighbor != null) {
+				neighbor[2] = true
+				neighbor[3] = 3 // Time to Live auf 3 Runden
+			}
+
+			int i = (int) neighbor[4]
+
+			// ANT aktualisieren
+			aliveNeighborTable.set(i,neighbor)
+
+			printANT()
+		}
+	}
+
+	/**
+	 * Verringern der Time to Live (TTL)
+	 */
+	void decreaseNeighborTTL() {
+		// Untersuchung aller Nachbarn in der ANT
+		for(List neighbor in aliveNeighborTable) {
+
+			// Wenn Nachbar noch am Leben, dann TTL verringern
+			if (neighbor[3] > 0) {
+				neighbor[3]--
+			}
+			// Ansonsten ist der Nachbar tot
+			else {
+				neighbor[2] = false
+			}
+
+			int i = (int) neighbor[4]
+
+			aliveNeighborTable.set(i,neighbor)
+
+			printANT()
 		}
 	}
 }
--- src/experiments/experiment1/hosts/router4/Router4.groovy
+++ src/experiments/experiment1/hosts/router4/Router4.groovy
@@ -25,8 +25,13 @@ class Router4 {
 
     /** Tabelle der IP-Adressen und UDP-Ports der Nachbarrouter */
     /*  z.B. [["1.2.3.4", 11],["5,6,7.8", 20]]
-     */
+        */
     List<List> neighborTable
+
+    /** Tabelle der IP-Adressen, UDP-Ports der Nachbarrouter, Lebensstatus, TTL */
+    /*  z.B. [["1.2.3.4", 11, true, 3],["5,6,7.8", 20, false, 0]]
+        */
+    List<List> aliveNeighborTable = []
 
     /** Eine Arbeitskopie der Routingtabelle der Netzwerkschicht */
     List routingTable
@@ -40,9 +45,7 @@ class Router4 {
      * Start der Anwendung
      */
     static void main(String[] args) {
-        // Router-Klasse instanziieren
         Router4 application = new Router4()
-        // und starten
         application.router()
     }
     //------------------------------------------------------------------------------
@@ -57,6 +60,17 @@ class Router4 {
 
         // Konfiguration holen
         config = Utils.getConfig("experiment1", "router4")
+        neighborTable = config.neighborTable
+
+        // Nachbarn in ANT übernehmen mit false und 0
+
+        int i = 0
+        for(List entry in neighborTable) {
+            List neighbor = [entry[0],entry[1],false,0,i++]
+            aliveNeighborTable.add(neighbor)
+        }
+
+        printANT()
 
         // ------------------------------------------------------------
 
@@ -71,12 +85,23 @@ class Router4 {
 
         // ------------------------------------------------------------
 
-        Utils.writeLog("Router4", "router4", "startet", 1)
+        Utils.writeLog("Router", "router1", "startet", 1)
 
         while (run) {
+
+            sendAliveRequest()
+            getAliveRequest()
+
+            getAliveReply()
+            // TTL verringern
+            decreaseNeighborTTL()
+
             // Periodisches Versenden von Routinginformationen
             sendPeriodical()
+
             sleep(config.periodRInfo)
+
+            // Status anfragen
         }
     }
 
@@ -87,6 +112,7 @@ class Router4 {
      *
      */
     void receiveFromNeigbor() {
+
         /** IP-Adresse des Nachbarrouters */
         String iPAddr
 
@@ -97,30 +123,22 @@ class Router4 {
         String rInfo
 
         // Auf UDP-Empfang warten
-        (iPAddr, port, rInfo) = stack.udpReceive()
-
-        // Jetzt aktuelle Routingtablle holen:
-        // rt = stack.getRoutingtable()
-        // neue Routinginformationen bestimmen
-        //    zum Zerlegen einer Zeichenkette siehe "tokenize()"
-        // extrahieren von Information, dann iInfo als !Zeichenkette! erzeugen ...
-        // Routingtabelle an Vermittlungsschicht uebergeben:
-        // stack.setRoutingtable(rt)
-        // und neue Routinginformationen verteilen:
-        // rInfo = ...
-        // sendToNeigbors(rInfo)
-        // oder periodisch verteilen lassen
+        (iPAddr, port, rInfo) =  stack.udpReceive()
     }
 
     // ------------------------------------------------------------
 
     /** Periodisches Senden der Routinginformationen */
     void sendPeriodical() {
-        // Paket mit Routinginformationen packen
-        // ... z.B.
-        // routingTable = stack.getRoutingTable()
-        // extrahieren von Information, dann iInfo als !Zeichenkette! erzeugen ...
-        String rInfo = "inf1a, inf1b, ..., inf2a, inf2b, ..."
+        routingTable = stack.getRoutingTable()
+
+        String rInfo = ""
+
+        for (List entry in routingTable) {
+            rInfo = rInfo + "${entry[0]}, ${entry[1]}, ${entry[2]}, ${entry[3]}; "
+        }
+
+        Utils.writeLog("Router", "router1", "rInfo: ${rInfo}",1)
 
         // Zum Senden uebergeben
         sendToNeigbors(rInfo)
@@ -132,14 +150,102 @@ class Router4 {
      *
      * @param rInfo - Routing-Informationen
      */
-
     void sendToNeigbors(String rInfo) {
         // rInfo an alle Nachbarrouter versenden
         for (List neigbor in neighborTable) {
-            stack.udpSend(dstIpAddr: neigbor[0], dstPort: neigbor[1],
-                    srcPort: config.ownPort, sdu: rInfo)
+            stack.udpSend(dstIpAddr: neigbor[0], dstPort: neigbor[1], srcPort: config.ownPort, sdu: rInfo)
         }
     }
-    //------------------------------------------------------------------------------
-}
 
+    //------------------------------------------------------------------------------
+
+    void printANT() {
+        for(entry in aliveNeighborTable) {
+            println("IP: ${entry[0]}, Port: ${entry[1]}, isAlive: ${entry[2]}, TTL: ${entry[3]}, Pos: ${entry[4]}")
+        }
+    }
+
+    void sendAliveRequest() {
+        for (List neigbor in neighborTable) {
+            //Utils.writeLog("Router 1", "send", "\u001B[32mAliveRequest\u001B[0m", 1)
+            println("\u001B[32mAliveRequest\u001B[0m")
+            stack.udpSend(dstIpAddr: neigbor[0], dstPort: neigbor[1], srcPort: config.ownPort, sdu: "alive")
+        }
+    }
+
+    void getAliveRequest() {
+        /** IP-Adresse des Nachbarrouters */
+        String iPAddr
+
+        /** UDP-Portnummer des Nachbarrouters */
+        int port
+
+        /** Empfangene Routinginformationen */
+        String rInfo
+
+        // Auf UDP-Empfang warten
+        (iPAddr, port, rInfo) =  stack.udpReceive()
+
+        if (rInfo.equals("alive")) {
+            Utils.writeLog("Router 1", "receive", "\u001B[32mAliveRequest\u001B[0m", 1)
+            sendAliveReply(iPAddr, port)
+        }
+    }
+
+    void sendAliveReply(String IpAddr, int port) {
+        stack.udpSend(dstIpAddr: IpAddr, dstPort: port, srcPort: config.ownPort, sdu: "ok")
+        Utils.writeLog("Router 1", "send", "\u001B[32mAliveReply\u001B[0m", 1)
+    }
+
+    void getAliveReply() {
+        String iPAddr
+        int port
+        String rInfo
+
+        // Auf UDP-Empfang warten
+        (iPAddr, port, rInfo) =  stack.udpReceive()
+
+        if (rInfo.equals("ok")) {
+
+            // Nachbarrouter ausfindig machen
+            List neighbor = aliveNeighborTable.find { entry -> entry[0].equals(iPAddr) }
+
+            // Nachbarrouter aktualisieren
+            if (neighbor != null) {
+                neighbor[2] = true
+                neighbor[3] = 3 // Time to Live auf 3 Runden
+            }
+
+            int i = (int) neighbor[4]
+
+            // ANT aktualisieren
+            aliveNeighborTable.set(i,neighbor)
+
+            printANT()
+        }
+    }
+
+    /**
+     * Verringern der Time to Live (TTL)
+     */
+    void decreaseNeighborTTL() {
+        // Untersuchung aller Nachbarn in der ANT
+        for(List neighbor in aliveNeighborTable) {
+
+            // Wenn Nachbar noch am Leben, dann TTL verringern
+            if (neighbor[3] > 0) {
+                neighbor[3]--
+            }
+            // Ansonsten ist der Nachbar tot
+            else {
+                neighbor[2] = false
+            }
+
+            int i = (int) neighbor[4]
+
+            aliveNeighborTable.set(i,neighbor)
+
+            printANT()
+        }
+    }
+}
